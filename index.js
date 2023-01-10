7363450a1e7ddee1974e6c3beec5acfbfaf8f369
sarahcharbit <sarahcharbit000@gmail.com>
2023-01-10 14:29:10 +0100

    add serveur back

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
 const serverPort = 4242
-
 const app = express()
+
+const data = require('./super.json')
 
 app.use(morgan('tiny'))
 app.use(cors('*'))
@@ -15,5 +15,15 @@ app.get('/', (req, res) => {
   console.log('une nouvelle requête est arrivée dans l’API !  ')
   res.json('je suis dans le /')
 })
-
+app.get('/catalogue', (req, res) => {
+  res.json(data)
+})
 app.listen(serverPort, () => console.log('http://localhost:4242'))
+app.get('/catalogue/vilain', (req, res) => {
+  const thing = data.filter(superMechant => superMechant.alignment === 'bad')
+  if (thing) {
+    res.send(thing)
+  } else {
+    console.log('404 ')
+  }
+})
