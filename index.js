28108d2657a95b49b7d3461e82b077940321dc84
JeanLoic GARCIA <(mailto:garcia.jeanloic@gmail.com)>
2023-01-25 14:00:37 +0100

    add miniatures and update json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(morgan('tiny'))
 app.use(cors('*'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+app.use(express.static('public'))
 app.get('/', (req, res) => {
   console.log('une nouvelle requête est arrivée dans l’API !  ')
   res.json('je suis dans le /')
